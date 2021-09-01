ba17afd23b86242b448f94830e31bd225a179be4
rene <41963722+renaynay@users.noreply.github.com>
2021-09-01 13:35:46 +0200

    draft

--- service/block/service.go
+++ service/block/service.go
@@ -1,5 +1,11 @@
 package block
 
+import (
+	"context"
+	"fmt"
+	"github.com/celestiaorg/celestia-node/rpc"
+)
+
 // Service represents the Block service that can be started / stopped on a `Full` node.
 // Service contains 4 main functionalities:
 //		1. Fetching "raw" blocks from Celestia-Core. // TODO note: this will eventually take place on the p2p level.
@@ -7,13 +13,39 @@ package block
 // 		3. Storing erasure coded blocks.
 // 		4. Serving erasure coded blocks to other `Full` node peers. // TODO note: optional for devnet
 type Service struct {
-			
+	rpc *rpc.Client
 }
 
-func NewBlockService() (*Service, error) {
-	return nil, nil
+func NewBlockService(rpc *rpc.Client) (*Service, error) {
+	if rpc == nil {
+		return nil, fmt.Errorf("no running rpc found")
+	}
+	serv := &Service{
+		rpc: rpc,
+	}
+	return serv, nil
 }
 
+func (s *Service) Start() error {
 
+	return nil
+}
 
+func (s *Service) newBlockListener(ctx context.Context) error {
+	newBlockEventChan, err := s.rpc.StartBlockSubscription(ctx)
+	if err != nil {
+		return err
+	}
 
+	for {
+		select {
+		case <- ctx.Done():
+
+		}
+	}
+}
+
+// TODO:
+// 	a function that has a listener loop for new events coming through the block subscription chan and then pipes them
+//	 into the erasure coding function which then produces DAH, verifies against data root in header of raw block, if good
+// 	gets stored and is ready to be served.
