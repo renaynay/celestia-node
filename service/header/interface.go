556d34e33b400047fba3c09877dfbe2edaa1c6c8
rene <41963722+renaynay@users.noreply.github.com>
2021-10-25 17:26:05 +0200

    subscriber

--- service/header/interface.go
+++ service/header/interface.go
@@ -2,7 +2,15 @@ package header
 
 import "context"
 
-// Exchange encompasses the behaviour necessary for a Node to
+// Subscriber encompasses the behaviour necessary for a node to
+// subscribe/unsubscribe from new ExtendedHeader events from the
+// network.
+type Subscriber interface {
+	Subscribe(ctx context.Context) (<-chan *ExtendedHeader, error)
+	Unsubscribe() error
+}
+
+// Exchange encompasses the behaviour necessary for a node to
 // retrieve and provide ExtendedHeaders to the Celestia network.
 type Exchange interface {
 	RetrieveHeaders(ctx context.Context, request *ExtendedHeaderRequest) ([]*ExtendedHeader, error)
