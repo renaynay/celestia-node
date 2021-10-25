b91cd4301ebc71d3bb3bc7c72fb8bbe6b4b837b4
rene <41963722+renaynay@users.noreply.github.com>
2021-10-25 12:38:08 +0200

    store interface

--- service/header/interface.go
+++ service/header/interface.go
@@ -5,6 +5,13 @@ import "context"
 // Exchange encompasses the behaviour necessary for a Node to
 // retrieve and provide ExtendedHeaders to the Celestia network.
 type Exchange interface {
-	Retrieve(ctx context.Context, request *ExtendedHeaderRequest) ([]*ExtendedHeader, error)
-	Provide(ctx context.Context, header *ExtendedHeader) error
+	RetrieveHeaders(ctx context.Context, request *ExtendedHeaderRequest) ([]*ExtendedHeader, error)
+	ProvideHeaders(ctx context.Context, header []*ExtendedHeader) error
+}
+
+// Store encompasses the behaviour necessary to store and retrieve ExtendedHeaders
+// from a node's local storage.
+type Store interface {
+	GetHeaders(ctx context.Context, request *ExtendedHeaderRequest) ([]*ExtendedHeader, error)
+	StoreHeaders(ctx context.Context, headers []*ExtendedHeader) error
 }
